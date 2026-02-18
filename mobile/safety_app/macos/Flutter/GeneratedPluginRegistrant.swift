31b285a3cd79d4f29c498a8733416f38d3fdb011
Sami <samikshya.baral021@apexcollege.edu.np>
2026-02-18 15:30:52 +0545

    liveaftermergeconflictresolve

--- mobile/safety_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile/safety_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,8 +13,8 @@ import firebase_database
 import firebase_messaging
 import flutter_local_notifications
 import flutter_secure_storage_darwin
-import local_auth_darwin
 import geolocator_apple
+import local_auth_darwin
 import mobile_scanner
 import package_info_plus
 import path_provider_foundation
@@ -33,6 +33,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
